dd89b04bee1fc968e9ca770a742393619cca9098
Vadymxz8 <vadim.tkach2017@gmil.com>
2025-03-16 22:31:11 +0100

    add_homework18

--- src/main/java/com/vadim/tkach/Main.java
+++ src/main/java/com/vadim/tkach/Main.java
@@ -1,7 +1,12 @@
 package com.vadim.tkach;
-
+import com.vadim.tkach.lesson18.Vehicle;
+import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Vehicle car = new Vehicle(10.0, 8.0, LocalDate.of(2025, 12, 31));
+
+        System.out.println("Може їхати: " + car.canDrive());
+        System.out.println("Допустима до експлуатації: " + car.isAllowedToOperate());
+        System.out.println("Максимальна відстань: " + car.getMaxDistance() + " км");
     }
 }
